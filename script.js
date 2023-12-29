0f94b25b63c05bdf2d328c67f54e19b118f0b9d6
Pradeep Bayereddy <vpradeepbayereddy@gmail.com>
2023-12-29 19:57:24 +0530

    Bug Fix

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ function beforeSubmit(event) {
   if (captchaChecked) {
     let outputDate = document.querySelector(".outputDate");
     let inputDate = document.querySelector(".inputDate"); //Date - type of String
-
     let formattedDate = new Date(inputDate.value).toLocaleDateString("en-IN");
     outputDate.value = formattedDate;
   } else {
